06e65fedd9e9255689cc0b145006e7deac990631
Péter Tircsi <46480345+petertircsi@users.noreply.github.com>
2019-05-20 08:28:43 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+function solution(a, b) {
+  return a + b;
+}
+
+export default solution;
---
+++ src/index.test.js
@@ -0,0 +1,5 @@
+import solution from "./index.js";
+
+it("it is working", () => {
+  expect(solution(1, 1)).toBe(2);
+});
